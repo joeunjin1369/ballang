9b03413ca8bdad3a9d04ec01dfd213ab77997e85
joeunjin1369 <joeunjin1369@gmail.com>
2024-10-02 17:19:18 +0900

    error correction

--- app/(providers)/(root)/cart/page.tsx
+++ app/(providers)/(root)/cart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Page from "../_components/Page/Page";
 import { useQuery } from "@tanstack/react-query";
 import api from "@/api/api";
